c69d29f4da0610878424b2caaceba4b5f4e2c3f6
navyxie <navyxie2010@gmail.com>
2013-04-09 00:32:27 +0800

    修复new多个dialog的bug,增加alert功能

--- public/js/jquery.dialog.js
+++ public/js/jquery.dialog.js
@@ -31,7 +31,7 @@ NAVY.Dialog = function(contentHtml,options){
         marginTop:null,//对话框距离容器右边的值
         dialogClass:'',//对话框的容器类
         position:'fixed',//对话框定位，默认为固定定位
-        autoClose:0//自动关闭对话框的时间，0表示不关闭,单位为秒
+        autoCloseSecond:0//自动关闭对话框的时间，0表示不关闭,单位为秒
     };
     this.contentHtml = contentHtml || '请填充对话框内容';
     $.extend(defaultOptions,options);
@@ -73,19 +73,20 @@ NAVY.Dialog.prototype = {
             _this.closeDialog();
         });
         //自动关闭
-        if(options.autoClose !== 0){
+        var autoCloseSecond = options.autoCloseSecond;
+        if(autoCloseSecond !== 0){
             if(closeDialogBtnObj.length){
-                closeDialogBtnObj.html('<span class="leftSecond">'+options.autoClose+'</span>秒后关闭')
+                closeDialogBtnObj.html('<span class="leftSecond">'+autoCloseSecond+'</span>秒后关闭');
+                var leftSecondDom = closeDialogBtnObj.find('.leftSecond')[0];
+                //倒计时
+                NAVY.UTIL.Date.countDown(leftSecondDom,autoCloseSecond)
             }
-            var leftSecondDom = closeDialogBtnObj.find('.leftSecond')[0];
             var setTimeOutId = setTimeout(function(){
                 _this.closeDialog();
                 if(setTimeOutId){
                     clearTimeout(setTimeOutId);
                 }
-            },options.autoClose*1000);
-            //倒计时
-            NAVY.UTIL.Date.countDown(leftSecondDom,options.autoClose)
+            },autoCloseSecond*1000);
         }
         return this;
     },
@@ -126,15 +127,28 @@ NAVY.Dialog.prototype = {
         }
         dialogHtmlArr.push('</td></tr></table></div>');
         /*-------------------- 拼接对话框的html end -----------------------*/
-        $(options.target).append(dialogHtmlArr.join(''));
-        _this.dialogObj = _this.targetObj.find('.navyDialogWrapper');
+        _this.dialogObj = $(dialogHtmlArr.join('')).appendTo(options.target);//每个对象都是对立的
+        //$(options.target).append(dialogHtmlArr.join(''));
+        //_this.dialogObj = _this.targetObj.find('.navyDialogWrapper');
         var dialogObj = _this.dialogObj;
         var dialogObjWidth = options.dialogWidth || dialogObj.outerWidth(),dialogObjHeight = options.dialogHeight || dialogObj.outerHeight();
-        //对话框最小宽度为130px
-        dialogObjWidth = dialogObjWidth < 130 ? 130 : dialogObjWidth;
-        var dialogObjMarginLeft = options.marginLeft || dialogObjWidth/2 , dialogObjMarginTop = options.marginTop || dialogObjHeight/2;
+        //对话框最小宽度为100px
+        dialogObjWidth = dialogObjWidth < 100 ? 100 : dialogObjWidth;
+        var dialogObjMarginLeft = dialogObjWidth/2 , dialogObjMarginTop = dialogObjHeight/2;
+        var marginLeft = options.marginLeft,marginTop = options.marginTop;
         //设置对话框的宽度和高度以及marginLeft和marginTop值
-        dialogObj.css({width:dialogObjWidth,height:dialogObjHeight,marginLeft:-dialogObjMarginLeft,marginTop:-dialogObjMarginTop,overflow:'hidden'});
+        if(marginLeft !== null){
+            if(marginTop !== null){
+                dialogObj.css({width:dialogObjWidth,height:dialogObjHeight,marginLeft:marginLeft,marginTop:marginTop,left:0,top:0,overflow:'hidden'});
+            }else{
+                dialogObj.css({width:dialogObjWidth,height:dialogObjHeight,marginLeft:marginLeft,marginTop:-dialogObjMarginTop,left:0,overflow:'hidden'});
+            }
+        }else if(marginTop !== null){
+            dialogObj.css({width:dialogObjWidth,height:dialogObjHeight,marginLeft:-dialogObjMarginLeft,marginTop:marginTop,top:0,overflow:'hidden'});
+        }else{
+            dialogObj.css({width:dialogObjWidth,height:dialogObjHeight,marginLeft:-dialogObjMarginLeft,marginTop:-dialogObjMarginTop,overflow:'hidden'});
+        }
+        //如果有遮罩层
         if(options.isMask){
             if(!(_this.bodyObj.find('.navyMaskWrapper').length)){
                 _this.bodyObj.append('<div style="z-index: '+(1+maxZindex)+'" class="navyMaskWrapper"></div>');
@@ -155,14 +169,60 @@ NAVY.Dialog.prototype = {
         return this;
     }
 };
-NAVY.Tip = {
-    success:function(options){
-
-    },
-    error:function(options){
-
-    },
-    warning:function(options){
-
+/**
+ * alert 提示框
+ * @param content alert提示的内容
+ * @param options alert的配置，默认是成功框包含属性如下：
+ * options = {
+         type : 'success',//alert框类型，默认是成功
+         dialogClass: 'navyDialogAlert',//增加alert的class,用于设置样式
+         autoCloseSecond:3,//自动关闭alert框，默认是3秒钟
+         position:'fixed',//alert的定位，默认是fixed
+         target:'body',//alert框的容器，默认是body
+         dialogWidth:null,//对话框的宽度
+         dialogHeight:null,//对话框的高度
+         marginLeft:null,//对话框距离容器左边的值
+         marginTop:null//对话框距离容器右边的值
+ * }
+ * @return {NAVY.Dialog} 返回对话框对象
+ * @constructor
+ */
+NAVY.Alert = function(content,options){
+    options = options || {};
+    var type = options.type || 'success';
+    var alertClass = 'successNavyAlert';
+    var dialogClass = options.dialogClass;
+    if(dialogClass){
+        dialogClass += ' navyDialogAlert';
+    }else{
+        dialogClass = 'navyDialogAlert';
     }
+    switch (type){
+        case 'error':
+            alertClass = 'errorNavyAlert';
+            break;
+        case 'warning':
+            alertClass = 'warningNavyAlert';
+            break;
+        default :
+            alertClass = 'successNavyAlert';
+            break;
+    }
+    content = '<div class="navyAlert '+alertClass+'">'+content+'</div>';
+    var alertOptions = {
+        isShowTiTle:false,//隐藏标题
+        isShowCloseBtn:false,//隐藏关闭按钮
+        isShowSureBtn:false,//隐藏确定按钮
+        isShowCancelBtn:false,//隐藏取消按钮
+        isMask:false,//隐藏遮罩层
+        dialogClass: dialogClass,//增加alert的class,用于设置样式
+        autoCloseSecond:options.autoCloseSecond || 3,//自动关闭alert框，默认是3秒钟
+        position:options.position || 'fixed',//alert的定位，默认是fixed
+        target:options.target || 'body',//alert框的容器，默认是body
+        dialogWidth:options.dialogWidth || null,//对话框的宽度
+        dialogHeight:options.dialogHeight || null,//对话框的高度
+        marginLeft:options.marginLeft || null,//对话框距离容器左边的值
+        marginTop:options.marginTop || null//对话框距离容器右边的值
+    };
+    return new NAVY.Dialog(content,alertOptions);
 };
